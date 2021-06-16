a4b3a2ac1ca04b3d66f2c5d34ab088df4ea7435f
tomipoitevin2103 <80926069+tomipoitevin2103@users.noreply.github.com>
2021-06-16 17:48:13 -0400

    Add files via upload

--- Proyecto-integrador-main/Javatfi/genres.js
+++ Proyecto-integrador-main/Javatfi/genres.js
@@ -1,1 +1,2 @@
-let generos
+let generos 
+
--- Proyecto-integrador-main/Javatfi/search-resultss.js
+++ Proyecto-integrador-main/Javatfi/search-resultss.js
@@ -52,6 +52,54 @@ if (campoBuscar.value == '') {
     formulario.submit()
 }
 
+}) 
+
+window.addEventListener('load', function () { 
+
+    console.log(window);
+    console.log(document);
+    console.log(location.search); 
+
+    let busqueda = document.querySelector(".Resultados"); 
+
+    let queryString = location.search;
+let queryStringobj= new URLSearchParams(queryString); 
+
+let cual = queryStringobj.get('buscar'); 
+console.log(cual); 
+
+let proxy = 'https://cors-anywhere.herokuapp.com/' ; 
+
+let search = 'https://api.deezer.com/search?q=' ; 
+
+let searchLink = proxy + search; 
+
+fetch( searchLink + cual)
+    .then(function (respuesta) {
+    return respuesta.json()
 })
 
+.then(function (datos){
+
+    let cosasBuscadas = datos.data
+
+    for (let i=0; i < cosasBuscadas.length; i++) { 
+
+        let bucle = cosasBuscadas[i];
+        let titulo = bucle.title; 
+
+        let bucle2 = cosasBuscadas[i]; 
+        let foto = bucle2.artist.picture;
+
+busqueda.innerHTML+= ` <a href="detallesartistas.html">
+<li> <img src="${foto}" > <h3> ${titulo} </h3></li>  `;
+
+ } })
+
+ .catch(function (error){
+    console.log(error);
+})
+
+
+})
 
