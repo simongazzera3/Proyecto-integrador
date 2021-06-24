d1bf40043ac4f7588d5fbb071061aa8e3b52e7a5
simongazzera3 <69983377+simongazzera3@users.noreply.github.com>
2021-06-24 17:40:03 -0300

    Add files via upload

--- Proyecto-integrador-main/Proyecto-integrador-main/Javatfi/index.js
+++ Proyecto-integrador-main/Proyecto-integrador-main/Javatfi/index.js
@@ -26,9 +26,8 @@ window.addEventListener('load', function() {
                 let imagen = cancion.artist.picture_big;
                 let title = cancion.title;
                 let nombreDeArtista = cancion.artist.name;
-                let idTema = cancion.id
 
-                lista.innerHTML += ` <a href="detallescanciones.html?id=${idTema}">
+                lista.innerHTML += ` <a href="detallescanciones.html">
                 <li> <img src="${imagen}" > <h3> ${title} </h3> <h3> - ${nombreDeArtista}</h3></li>
                 `
             }
@@ -63,7 +62,6 @@ window.addEventListener('load', function() {
                 let imagenAlbum = cancion.artist.picture_big;
                 let title = cancion.title;
                 let nombreDeArtista = cancion.artist.name;
-                let idTema = cancion.id
 
 
                 console.log(title);
@@ -71,7 +69,7 @@ window.addEventListener('load', function() {
                 //console.log(imagen);
 
 
-                lista.innerHTML += ` <a href="detallesalbum.html?id=${idTema}">
+                lista.innerHTML += ` <a href="detallesalbum.html">
                 <li> <img src="${imagenAlbum}" > <h3> ${title} </h3> <h3> - ${nombreDeArtista}</h3> </li>
                 `
             }
@@ -107,14 +105,13 @@ window.addEventListener('load', function() {
                 let cancion = artistas[i];
                 let imagenArtistas = cancion.picture_big;
                 let title = cancion.name;
-                let idTema = cancion.id
 
                 console.log(title);
 
                 console.log(cancion);
 
 
-                lista.innerHTML += ` <a href="detallesartistas.html?id=${idTema}">
+                lista.innerHTML += ` <a href="detallesartistas.html">
                 <li> <img src="${imagenArtistas}" > <h3> ${title} </h3> </li>
                 `
             }
