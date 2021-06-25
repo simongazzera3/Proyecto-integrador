2fa852557d3322622a378e6a088f528a371381e7
tomipoitevin2103 <tgarciapoitevin@udesa.edu.ar>
2021-06-25 17:56:44 -0400

    Merge branch 'main' of https://github.com/simongazzera3/Proyecto-integrador

--- Javatfi/detallesartistas.js
+++ Javatfi/detallesartistas.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let search_results = new URLSearchParams(this.location.search);
 let codigo = search_results.get(`id`);
 console.log(`id: ` + codigo);
@@ -37,43 +36,3 @@ for (let i = 0; i < 5; i++) {
 
 }) 
 
-=======
-let search_results = new URLSearchParams(this.location.search);
-let codigo = search_results.get(`id`);
-console.log(`id: ` + codigo);
-
-fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${codigo}`)
-.then(function(respuestas) {
-    return respuestas.json();
-})
-.then(function(data) {
-    console.log(data);
-    let fotoArtista = data.picture
-    let nombreArtista = data.name
-    let ubic = document.querySelector(`.textodetalles`)
-
-    ubic.innerHTML +=`
-            <h1>${nombreArtista}</h1>
-            <img src="${fotoArtista}" alt="${nombreArtista}">
-        `
-}) 
-
-fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${codigo}/albums`)
-.then(function(respuestas) {
-    return respuestas.json();
-})
-.then(function(data) {
-    let album = data.data
-    let ubic = document.querySelector(`.listadetallesartista`)
-
-for (let i = 0; i < 5; i++) {
-    let fotosalbums = album[i].cover_medium
-
-    ubic.innerHTML +=`
-    <img src="${fotosalbums}" alt="">
-`
-}
-
-}) 
-
->>>>>>> b41ff5d89e51ba61a5c1e97dff6319d0ad29db99
