fa4b1c63b41bbd4de0a179fdcf06f2a1341d4910
Luciofergut <80979212+Luciofergut@users.noreply.github.com>
2021-06-25 13:23:47 -0300

    Add files via upload

--- Proyecto-integrador-main/Javatfi/genres.js
+++ Proyecto-integrador-main/Javatfi/genres.js
@@ -27,49 +27,12 @@ fetch(urlGeneros)
             console.log(title);
             console.log(genero);
 
-            openPage = function() {
-                location.href = "detallesgeneros.html?id=" + id;
-            }
-
-            lista.innerHTML += `<li style="float:left; margin-left:50px;margin-top:20px;"><a href="javascript:openPage()">
-                <img style="max-width:150px;" src="${imagen}"> <h3> ${title} </h3></a></li>
-                `;
+            var gen = `<li style="float:left; margin-left:50px;margin-top:20px;" id="` + id + `"><a href="detallesgeneros.html?id=` + id + `">
+            <img style="max-width:150px;" src="${imagen}"> <h3> ${title} </h3></a></li>
+            `;
+            lista.innerHTML += gen;
         }
 
 
 
     })
-    /*
-    let apilink2 = "https://api.deezer.com/genre/0/artists";
-
-    let urlDetalles = proxy + apilink2;
-
-    fetch(urlDetalles)
-        .then(function(respuesta) {
-
-            return respuesta.json();
-
-        })
-        .then(function(datos) {
-
-            let generos = datos.data;
-
-            let lista = document.querySelector(".listageneros");
-
-
-            for (let i = 1; i < generos.length; i++) {
-                let genero = generos[i];
-                let imagen = genero.picture_big;
-                let title = genero.name;
-
-                console.log(title);
-                console.log(genero);
-
-                lista.innerHTML += `<li style="float:left; margin-left:50px;margin-top:20px;"><a href="detallesgeneros.html">
-                    <img style="max-width:150px;" src="${imagen}"> <h3> ${title} </h3></a></li>
-                    `;
-            }
-
-
-
-        })*/
